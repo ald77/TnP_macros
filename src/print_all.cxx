260e9705c89d3700c1ce91dde702ced212f7fc20
Adam Dishaw <aldishaw@gmail.com>
2015-08-11 13:50:14 -0700

    Make more flexible and allow full printing of all plots

--- src/print_all.cxx
+++ src/print_all.cxx
@@ -9,10 +9,7 @@
 
 using namespace std;
 
-void PrintDirectory(TDirectory &dir, const TString &set, const TString &measured,
-		    const TString &wp, const TString &ext = ""){
-  TString lower_wp = wp;
-  lower_wp.ToLower();
+void PrintDirectory(TDirectory &dir, const TString &ext = ""){
   TList *keys = dir.GetListOfKeys();
   if(keys == NULL) return;
   for(int i = 0; i < keys->GetSize(); ++i){
@@ -26,48 +23,52 @@ void PrintDirectory(TDirectory &dir, const TString &set, const TString &measured
     if(class_name == "TCanvas"){
       TCanvas *canvas = static_cast<TCanvas*>(obj);
       if(canvas == NULL) continue;
-      canvas->Print("plots/"+set+"_"+measured+"_"+lower_wp+"_"+ext+name+".pdf");
+      TString out_name = ext+"_"+name+".pdf";
+      out_name.ReplaceAll("/","_");
+      canvas->Print("plots/"+out_name);
     }else if(class_name.Contains("TDirectory")){
       TDirectory *sub_dir = static_cast<TDirectory*>(obj);
       if(sub_dir == NULL) continue;
-      PrintDirectory(*sub_dir, set, measured, wp, ext+name+"_");
+      PrintDirectory(*sub_dir, ext+"_"+name);
     }
   }
 }
 
-void PrintPlots(const TString& set, const TString& measured, const TString &wp){
-  TString lower_wp = wp;
-  lower_wp.ToLower();
-  TFile file(measured == "id" ? "data/eff_"+set+"_id.root"
-	     : "data/eff_"+set+"_"+lower_wp+".root","read");
-  TString path = "GsfElectronToID/MCtruth_"+wp;
-  if(measured != "id"){
-    path.ReplaceAll("GsfElectronToID",wp+"ElectronToMiniIso");
+void PrintPlots(TString file_name){
+  TFile file(file_name, "read");
+  file_name.ReplaceAll(".root","");
+  file_name.ReplaceAll("data/eff_","");
+  TList *keys = file.GetListOfKeys();
+  if(keys == NULL) return;
+  for(int i = 0; i < keys->GetSize(); ++i){
+    TObject *key = keys->At(i);
+    if(key == NULL) continue;
+    TString name = key->GetName();
+    if(name == "w") continue;
+    TObject *obj = file.Get(name);
+    if(obj == NULL) continue;
+    TString class_name = obj->ClassName();
+    if(class_name == "TCanvas"){
+      TCanvas *canvas = static_cast<TCanvas*>(obj);
+      if(canvas == NULL) continue;
+      canvas->Print("plots/"+file_name+name+".pdf");
+    }else if(class_name.Contains("TDirectory")){
+      TDirectory *sub_dir = static_cast<TDirectory*>(obj);
+      if(sub_dir == NULL) continue;
+      PrintDirectory(*sub_dir, file_name);
+    }
   }
-  if(set != "mc"){
-    path.ReplaceAll("MCtruth_","");
-  }
-  TDirectory *dir = static_cast<TDirectory*>(file.Get(path));
-  if(dir == NULL) return;
-  PrintDirectory(*dir, set, measured, wp);
 }
 
 int main(){
-  vector<TString> set, measured, wp;
-  set.push_back("mc");
-  set.push_back("data");
-  measured.push_back("id");
-  measured.push_back("iso");
-  wp.push_back("Veto");
-  wp.push_back("Loose");
-  wp.push_back("Medium");
-  wp.push_back("Tight");
-
-  for(size_t iset = 0; iset < set.size(); ++iset){
-    for(size_t imeasured = 0; imeasured < measured.size(); ++imeasured){
-      for(size_t iwp = 0; iwp < wp.size(); ++iwp){
-	PrintPlots(set.at(iset), measured.at(imeasured), wp.at(iwp));
-      }
-    }
-  }
+  PrintPlots("data/eff_data_id.root");
+  PrintPlots("data/eff_data_veto.root");
+  PrintPlots("data/eff_data_loose.root");
+  PrintPlots("data/eff_data_medium.root");
+  PrintPlots("data/eff_data_tight.root");
+  PrintPlots("data/eff_mc_id.root");
+  PrintPlots("data/eff_mc_veto.root");
+  PrintPlots("data/eff_mc_loose.root");
+  PrintPlots("data/eff_mc_medium.root");
+  PrintPlots("data/eff_mc_tight.root");
 }
